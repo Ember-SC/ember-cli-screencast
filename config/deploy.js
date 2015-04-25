35885b4887aa41dcfe104857f31f50a146c61416
Bill Heaton <bill@mediaartslab.com>
2015-04-25 14:46:29 -0700

    use env variables in the deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -1,16 +1,18 @@
+var pkg = require('../package.json');
+
 module.exports = {
   production: {
     store: {
       type: 'redis',
-      host: 'pixelhandler.com',
-      port: 6379,
-      password: process.env['PIXHLR_REDIS_PWORD']
+      host: process.env['REDIS_HOST'],
+      port: process.env['REDIS_PORT'],
+      password: process.env['REDIS_SECRET']
     },
     assets: {
       type: 's3',
       accessKeyId: process.env['AWS_ACCESS_KEY'],
       secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      bucket: 'chat-app'
+      bucket: process.env['APP_NAME'] || pkg.name
     }
   }
 };
